eedd7fee8bc9ca944d0fec949f7de9b17c9488ac
Abstract-ak <ash.king0205@gmail.com>
2024-12-23 18:22:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const urlRoute = require("./routes/url");
+const { connectToMongoDB } = require("./connect");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(
+  console.log("MongoDB Connected :)")
+);
+
+app.use(express.json());
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
