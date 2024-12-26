08197f6d13b4135d2e96b91081437c3879875abf
Abstract-ak <ash.king0205@gmail.com>
2024-12-26 15:57:50 +0530

    set cookie in handleloginuser

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+import { v4 as uuidv4 } from "uuid";
+import { setUser } from "../service/auth";
 
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
@@ -16,6 +18,10 @@ async function handleUserlogin(req, res) {
   const user = await User.findOne({ email, password });
   // console.log("user", user);
   if (!user) return res.render("login", { error: "Invalid Credentials!" });
+
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie("uid", sessionId);
 
   return res.redirect("/");
 }
---
+++ service/auth.js
@@ -0,0 +1,16 @@
+//User mapping Diary
+
+const sessionIdToUserMap = new Map();
+
+function setUser(id, user) {
+  sessionIdToUserMap.set(id, user);
+}
+
+function getuser(id) {
+  return sessionIdToUserMap.get(id);
+}
+
+module.exports = {
+  setUser,
+  getuser,
+};
